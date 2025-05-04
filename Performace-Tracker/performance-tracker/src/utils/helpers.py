1018797f1573be5f4ba2d4835b5ec6f173f364cc
ashrithapaidi123 <ashrithapaidi@gmail.com>
2025-05-04 11:10:50 +0530

    Delete Performace-Tracker directory

--- Performace-Tracker/performance-tracker/src/utils/helpers.py
+++
@@ -1,6 +0,0 @@
-def hash_password(password):
-    import hashlib
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def verify_password(stored_password, provided_password):
-    return stored_password == hash_password(provided_password)
